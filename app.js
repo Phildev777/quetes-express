eedec81c57a1ccae7169f6da4063a35618bcc0f6
Philippe Berra <berraphilippe07@gmail.com>
2022-11-03 09:39:38 +0100

    solution quête 2 express

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
+
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
+const usersHandlers = require("./usersHandlers");
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
 
 const movieHandlers = require("./movieHandlers");
 
